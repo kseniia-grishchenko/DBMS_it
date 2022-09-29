3a9b94b78898d36f18f40ad95867e2f08495c84a
kseniia <kseniia@smartiot.cloud>
2022-09-29 23:16:12 +0300

    add methods for printing rows

--- Row.py
+++ Row.py
@@ -4,3 +4,10 @@ class Row:
 
     def __getitem__(self, index):
         return self.values[index]
+
+    def __str__(self):
+        return f'[{", ".join(map(str, self.values))}]'
+
+    def __repr__(self):
+        return f'[{", ".join(map(str, self.values))}]'
+
