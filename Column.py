24a3d1a30e93d26afe41e103789a5b50fa4e98ee
kseniia <kseniia@smartiot.cloud>
2022-10-01 21:18:58 +0300

    add default value for each column type

--- Column.py
+++ Column.py
@@ -3,8 +3,10 @@ import re
 
 
 class Column(ABC):
-    def __init__(self, name):
+    def __init__(self, column_type, name, default):
+        self.type = column_type
         self.name = name
+        self.default = default
 
     def __str__(self):
         return self.name
@@ -18,28 +20,44 @@ class Column(ABC):
 
 
 class IntCol(Column):
+    default = 0
     type = 'int'
+
+    def __init__(self, name):
+        super().__init__(IntCol.type, name, IntCol.default)
 
     def validate(self, value):
         return isinstance(value, int)
 
 
 class RealCol(Column):
+    default = 0.0
     type = 'real'
+
+    def __init__(self, name):
+        super().__init__(RealCol.type, name, RealCol.default)
 
     def validate(self, value):
         return isinstance(value, float)
 
 
 class CharCol(Column):
+    default = ''
     type = 'char'
+
+    def __init__(self, name):
+        super().__init__(CharCol.type, name, CharCol.default)
 
     def validate(self, value):
         return isinstance(value, str) and len(value) == 1
 
 
 class StringCol(Column):
+    default = ''
     type = 'string'
+
+    def __init__(self, name):
+        super().__init__(StringCol.type, name, StringCol.default)
 
     def validate(self, value):
         return isinstance(value, str)
@@ -49,7 +67,7 @@ class EnumCol(Column):
     type = 'enum'
 
     def __init__(self, name, values):
-        super().__init__(name)
+        super().__init__(EnumCol.type, name, values[0])
         self.availableValues = values
 
     def validate(self, value):
@@ -57,7 +75,11 @@ class EnumCol(Column):
 
 
 class EmailCol(Column):
+    default = ''
     type = 'email'
+
+    def __init__(self, name):
+        super().__init__(EmailCol.type, name, EmailCol.default)
 
     def validate(self, value):
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
