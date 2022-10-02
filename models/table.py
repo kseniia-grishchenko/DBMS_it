8c82f8e936560b8c09ace6fa120122292c8168b5
kseniia <kseniia@smartiot.cloud>
2022-10-02 13:28:04 +0300

    Refactored logic of working with table, adding row fixes

--- models/table.py
+++ models/table.py
@@ -1,74 +1,78 @@
+from typing import Any
+
 from tabulate import tabulate
 
-from column import IntCol, RealCol, CharCol, StringCol, EnumCol, EmailCol
-from row import Row
-
-COLUMN_TYPES = ["int", "real", "char", "string", "enum", "email"]
+from .column import Column
+from .row import Row
 
 
 class Table:
-    columns = []
-    rows = []
-
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = name
+        self.columns: list[Column] = []
+        self.rows: list[Row] = []
 
     def __str__(self):
+        return f"Table: {self.name}\n" + self._str_columns_and_rows()
+
+    def _str_columns_and_rows(self) -> str:
+        if len(self.columns) == 0:
+            return ""
+
         return tabulate(
-            [row.values for row in self.rows],
-            [column.name for column in self.columns],
+            [[index] + row.values for index, row in enumerate(self.rows)],
+            ("index",) + self._get_column_names(),
             tablefmt="orgtbl",
         )
 
-    def add_row(self, values):
-        row = []
-        if len(self.columns) == 0:
-            raise ValueError("No column in table")
-        if len(values.keys()) > len(self.columns):
-            raise ValueError("Values length is bigger then column length")
-        for index, column in enumerate(self.columns):
-            current_value = values.get(column.name, None)
-            if not current_value:
-                default_value = column.default
-            if current_value and not column.validate(current_value):
-                raise TypeError(
-                    f"This value {current_value} does not match column type! "
-                    f"Column {column.name} type is {column.type} and entered type is {type(current_value)}"
-                )
-            row.append(current_value or default_value)
-        new_row = Row(row)
-        self.rows.append(new_row)
+    def _get_column_names(self) -> tuple[str]:
+        return tuple(column.name for column in self.columns)
 
-    def add_column(self, column_type, name, values=None):
-        if values is None:
-            values = []
-        if column_type not in COLUMN_TYPES:
-            raise TypeError(
-                "This type is not allowed! Please use one of the next ones: 'int', 'real', 'char', "
-                "'string', 'enum', 'email'"
+    def _check_column_name_already_exists(self, new_column_name: str) -> bool:
+        return new_column_name in self._get_column_names()
+
+    def add_column(self, column: Column) -> None:
+        if self._check_column_name_already_exists(column.name):
+            raise ValueError(
+                f"Column with name '{column.name}' already exists in the table!"
             )
-        column = self.create_column(name, column_type, values)
+
         self.columns.append(column)
+        self._add_default_values_to_all_existing_rows(column)
 
-    def create_column(self, name, column_type, values=None):
-        if values is None:
-            values = []
-        if column_type == "int":
-            return IntCol(name)
-        elif column_type == "real":
-            return RealCol(name)
-        elif column_type == "char":
-            return CharCol(name)
-        elif column_type == "string":
-            return StringCol(name)
-        elif column_type == "enum":
-            return EnumCol(name, values)
-        elif column_type == "email":
-            return EmailCol(name)
-        else:
-            raise TypeError("This type is not supported!")
+    def _add_default_values_to_all_existing_rows(self, column: Column) -> None:
+        for row in self.rows:
+            row.values.append(column.default)
 
-    def get_row(self, index: int):
+    def _validate_row_data(self, data: dict[str, Any]) -> None:
+        if len(data) == 0:
+            raise ValueError("Row data cannot be empty!")
+
+        columns_names = self._get_column_names()
+        if not set(data.keys()).issubset(set(columns_names)):
+            raise ValueError(
+                f"Invalid column names: {tuple(data.keys())} is not subset of {columns_names}!"
+            )
+
+    def add_row(self, data: dict[str, Any]) -> None:
+        self._validate_row_data(data)
+
+        row = []
+
+        for column in self.columns:
+            value_to_add = data.get(column.name, None)
+
+            if not value_to_add:  # handle default case
+                row.append(column.default)
+                continue
+
+            column.validate_or_error(value_to_add)
+
+            row.append(value_to_add)
+
+        self.rows.append(Row(row))
+
+    def get_row(self, index: int) -> Row:
         if not (0 <= index < len(self.rows)):
             raise ValueError("Row with such index does not exist!")
 
