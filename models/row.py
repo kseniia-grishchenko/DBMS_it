75329a29157a5291ec46e203cc2bde863f7a7201
kseniia <kseniia@smartiot.cloud>
2022-10-02 13:27:30 +0300

    Added __setitem__ for Row

--- models/row.py
+++ models/row.py
@@ -8,5 +8,8 @@ class Row:
     def __getitem__(self, index: int) -> Any:
         return self.values[index]
 
+    def __setitem__(self, key: int, value: Any) -> None:
+        self.values[key] = value
+
     def __repr__(self) -> str:
         return f'[{", ".join(map(str, self.values))}]'
