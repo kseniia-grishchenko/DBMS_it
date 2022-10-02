dbba2c40202a476e910b1577dcf786cab3f77f1e
kseniia <kseniia@smartiot.cloud>
2022-10-02 19:02:33 +0300

    table_name -> name refactoring

--- models/database.py
+++ models/database.py
@@ -14,22 +14,22 @@ class Database:
     def tables_count(self):
         return len(self._tables)
 
-    def add_table(self, table_name: str) -> None:
-        table = Table(table_name)
+    def add_table(self, name: str) -> None:
+        table = Table(name)
         if table.name in self._tables:
             raise ValueError(f"Table with name '{table.name}' already exists in DB!")
 
         self._tables[table.name] = table
 
-    def _validate_table_existence(self, table_name: str) -> None:
-        if table_name not in self._tables:
-            raise KeyError(f"No table with name '{table_name}' found in DB!")
+    def _validate_table_existence(self, name: str) -> None:
+        if name not in self._tables:
+            raise KeyError(f"No table with name '{name}' found in DB!")
 
-    def get_table(self, table_name: str) -> Table:
-        self._validate_table_existence(table_name)
-        return self._tables[table_name]
+    def get_table(self, name: str) -> Table:
+        self._validate_table_existence(name)
+        return self._tables[name]
 
-    def delete_table(self, table_name: str) -> Table:
-        table = self.get_table(table_name)
-        del self._tables[table_name]
+    def delete_table(self, name: str) -> Table:
+        table = self.get_table(name)
+        del self._tables[name]
         return table
