d97791625d2e6ab51203578adc94edd12d25dcf2
kseniia <kseniia@smartiot.cloud>
2022-10-24 22:32:59 +0300

    update db_manager interface

--- pyro_example/db_manager.py
+++ pyro_example/db_manager.py
@@ -2,18 +2,26 @@ from __future__ import annotations
 import os
 
 import pickle
+
+import Pyro5
 from Pyro5.api import expose, Daemon
 
 from typing import Any
 
-from models.column import Column
+from models import column
 from models.database import Database
 from models.row import Row
 from models.table import Table
 
 from models.db_manager import DBManager
 
-os.environ["PYRO_SERIALIZER"] = "pickle"
+one_input_columns = {
+    "int": column.IntCol,
+    "real": column.RealCol,
+    "char": column.CharCol,
+    "string": column.StringCol,
+    "email": column.EmailCol,
+}
 
 
 @expose
@@ -21,34 +29,52 @@ class RemoteDBManager(DBManager):
     def __init__(self, db: Database = None) -> None:
         self.db = db
 
+    @property
+    def db(self) -> Database:
+        if self._db is None:
+            raise ValueError("You should open or create DB before accessing it!")
+
+        return self._db
+
+    @db.setter
+    def db(self, value: Database) -> None:
+        self._db = value
+
     def create_database(self, name: str) -> None:
         self.db = Database(name)
         print(f"Database created: {name}")
-        return daemon.register(self.db)
+        daemon.register(self.db)
 
-    def add_table(self, name: str) -> None:
-        self.db.add_table(name)
+    def add_table(self, name: str) -> Table:
+        table = self.db.add_table(name)
+        table_uri = daemon.register(table)
         print(f"Table added: {name}")
+        return table_uri
 
     def get_table(self, name: str) -> Table:
         table = self.db.get_table(name)
         print(f"Get table: {name}", table)
-        return daemon.register(table)
+        return table
 
     def delete_table(self, name: str) -> Table:
         table = self.db.delete_table(name)
         print(f"Delete table: {name}")
-        return table
+        return daemon.unregister(table)
 
-    def add_column(self, table_name: str, column: Column) -> None:
+    def add_column(self, table_name: str, column_type, column_name) -> None:
+        new_column = one_input_columns[column_type](column_name)
         table = self.get_table(table_name)
-        table.add_column(column)
-        print(f"Add column: {column.name} to table: {table_name}")
+        added_column = table.add_column(new_column)
+        column_uri = daemon.register(added_column)
+        print(f"Add column: {new_column.name} to table: {table_name}")
+        return column_uri
 
     def add_row(self, table_name: str, data: dict[str, Any]) -> None:
         table = self.get_table(table_name)
-        table.add_row(data)
+        row = table.add_row(data)
+        row_uri = daemon.register(row)
         print(f"Add row: {data} to table: {table_name}")
+        return row_uri
 
     def change_row(self, table_name: str, index: int, data: dict[str, Any]) -> None:
         table = self.get_table(table_name)
@@ -58,15 +84,16 @@ class RemoteDBManager(DBManager):
     def find_rows(self, table_name: str, search_string: str) -> Table:
         table = self.get_table(table_name)
         print(f"Find rows of table: {table_name }by substring: {search_string} ")
-        rows = table.find_rows(search_string)
-        print(f"Found rows: {rows}")
-        return rows
+        view = table.find_rows(search_string)
+        view_uri = daemon.register(view)
+        print(f"Found rows: {view}")
+        return view_uri
 
     def delete_row(self, table_name: str, index: int) -> Row:
         table = self.get_table(table_name)
         row = table.delete_row(index)
         print(f"Delete row: {index} from table: {table_name}")
-        return row
+        return daemon.unregister(row)
 
     def save_database(self, path_to_save: str = None) -> str:
         if path_to_save is None:
@@ -83,9 +110,24 @@ class RemoteDBManager(DBManager):
         with open(path_to_load, "rb") as file:
             db = pickle.load(file)
 
+        print("opened db", db)
+
         self.db = db
+        daemon.register(self.db, force=True)
+
+        print("tables", db.tables)
+        for table in db.tables.values():
+            daemon.register(table, force=True)
+            for col in table.columns_obj:
+                daemon.register(col, force=True)
+            for row in table.rows_obj:
+                daemon.register(row, force=True)
 
         print(f"Open DB from: {path_to_load}")
+
+    def unregister_db(self) -> None:
+        print("unregister")
+        daemon.unregister(self.db)
 
 
 daemon = Daemon()
@@ -93,3 +135,4 @@ uri = daemon.register(RemoteDBManager)
 
 print("Ready. Object uri =", uri)
 daemon.requestLoop()
+# /Users/ksishka/Documents/university/4nd_course/it/dbms/pyro_example/temp
